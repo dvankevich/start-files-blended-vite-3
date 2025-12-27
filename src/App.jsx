a96d985d74aeebd32bd077a816d0e544a30c20f4
dvankevich <dvankevich@gmail.com>
2025-12-27 20:44:14 +0200

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
 import Header from './components/Header/Header';
+import { Suspense, lazy } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import Loader from './components/Loader/Loader';
+
+const Home = lazy(() => import('./pages/Home'));
+const SearchCountry = lazy(() => import('./pages/SearchCountry'));
+const Country = lazy(() => import('./pages/Country'));
 
 export const App = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    console.log('Current location:', location);
+  }, [location]);
   return (
     <>
       <Header />
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/country" element={<SearchCountry />} />
+          <Route path="/country/:countryId" element={<Country />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </Suspense>
     </>
   );
 };
--- src/pages/SearchCountry.jsx
+++ src/pages/SearchCountry.jsx
@@ -1,12 +1,14 @@
 import Container from '../components/Container/Container';
 import Heading from '../components/Heading/Heading';
 import Section from '../components/Section/Section';
+import SearchForm from '../components/SearchForm/SearchForm';
 
 const SearchCountry = () => {
   return (
     <Section>
       <Container>
         <Heading title="SearchCountry" bottom />
+        <SearchForm />
       </Container>
     </Section>
   );
